aa83154b3809ca34809185845538208c132faae7
georgemmp <machadogeorge.p@gmail.com>
2020-12-15 21:11:00 -0300

    RestTemplate

---
+++ hrpayroll/src/main/java/com/george/hrpayroll/entities/Worker.java
@@ -0,0 +1,47 @@
+package com.george.hrpayroll.entities;
+
+import java.io.Serializable;
+
+public class Worker implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String name;
+	private Double dailyIncome;
+
+	public Worker() {
+
+	}
+
+	public Worker(Long id, String name, Double dailyIncome) {
+		this.id = id;
+		this.name = name;
+		this.dailyIncome = dailyIncome;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Double getDailyIncome() {
+		return dailyIncome;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setDailyIncome(Double dailyIncome) {
+		this.dailyIncome = dailyIncome;
+	}
+
+}
