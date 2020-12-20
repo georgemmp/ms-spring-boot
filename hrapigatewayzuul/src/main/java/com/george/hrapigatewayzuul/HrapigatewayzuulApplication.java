7485b43e47644c3fce5945a95f7fd6d7f4b60a9c
georgemmp <machadogeorge.p@gmail.com>
2020-12-20 14:09:43 -0300

    Project API gateway zuul created

---
+++ hrapigatewayzuul/src/main/java/com/george/hrapigatewayzuul/HrapigatewayzuulApplication.java
@@ -0,0 +1,13 @@
+package com.george.hrapigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrapigatewayzuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrapigatewayzuulApplication.class, args);
+	}
+
+}
