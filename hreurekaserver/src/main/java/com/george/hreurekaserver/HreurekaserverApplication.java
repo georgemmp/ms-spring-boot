3d5bc1a9c2584be8e4a13c6b36743a34aac66986
georgemmp <machadogeorge.p@gmail.com>
2020-12-20 12:15:31 -0300

    Project eureka server created

---
+++ hreurekaserver/src/main/java/com/george/hreurekaserver/HreurekaserverApplication.java
@@ -0,0 +1,13 @@
+package com.george.hreurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HreurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HreurekaserverApplication.class, args);
+	}
+
+}
