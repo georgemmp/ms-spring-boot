c267961d4a5253d3401e75f9d8fd45acbea271bd
georgemmp <machadogeorge.p@gmail.com>
2020-12-24 14:19:18 -0300

    Created config server

---
+++ hrconfigserver/src/test/java/com/george/hrconfigserver/HrconfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.george.hrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrconfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
