d77fd0794910aed5c4fced8e08cff25b350577de
georgemmp <machadogeorge.p@gmail.com>
2020-12-25 16:59:11 -0300

    Added seed

---
+++ hruser/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO tb_user (name, email, password) VALUES ('Joselito', 'joselito@mail.com', '$2a$10$lZXlkBqX4unyZUFfKVpck.DeNgEuf9M.BywmAv/g./Oo4if.8lpg6');
+INSERT INTO tb_user (name, email, password) VALUES ('Leia Vermelho', 'leiav@mail.com', '$2a$10$lZXlkBqX4unyZUFfKVpck.DeNgEuf9M.BywmAv/g./Oo4if.8lpg6');
+
+INSERT INTO tb_role (role_name) VALUES ('ROLE_OPERATOR');
+INSERT INTO tb_role (role_name) VALUES ('ROLE_ADMIN');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
