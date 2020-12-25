6518b6a1e1a340037354db81c7a4e76f84934618
georgemmp <machadogeorge.p@gmail.com>
2020-12-25 16:02:54 -0300

    Project hruser created

---
+++ hruser/src/test/java/com/george/hruser/HruserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.george.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HruserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
